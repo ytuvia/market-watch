1f343c35bf61f76ae99a0389444de37e7648e889
Ubuntu <ubuntu@ip-172-31-20-59.us-west-2.compute.internal>
2023-12-12 16:04:13 +0000

    finished move to deebee account

---
+++ amplify/backend/function/runAssistantTrigger/src/index.js
@@ -0,0 +1,24 @@
+/* Amplify Params - DO NOT EDIT
+	API_MARKETWATCH_GRAPHQLAPIENDPOINTOUTPUT
+	API_MARKETWATCH_GRAPHQLAPIIDOUTPUT
+	API_MARKETWATCH_GRAPHQLAPIKEYOUTPUT
+	ENV
+	REGION
+	STORAGE_CONTENT_BUCKETNAME
+Amplify Params - DO NOT EDIT */
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+};
